cb1a0f97ff8d20e6cfe3a3bcaecbc35f56d2cba6
Kevin Greiner <greinerk@gmail.com>
2015-02-21 20:39:23 -0500

    multiples of 3 or 5
    sum the multiples of 3 or 5

---
+++ solutions/cs/multiples-of-3-or-5.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace MultiplesOf3or5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    sum += i;
+            }
+            Console.WriteLine("{0}", sum);
+        }
+    }
+}
